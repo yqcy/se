b2aebc1df5334b74d867910b4a4cabc9e1c33af2
wb-yq264139 <wb-yq264139@alibaba-inc.com>
2017-05-23 10:09:38 +0800

    update the user_detail.html

---
+++ src/main/java/com/yq/se/shiro/ShiroConfiguration.java
@@ -0,0 +1,80 @@
+package com.yq.se.shiro;
+
+import org.apache.shiro.cache.ehcache.EhCacheManager;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.spring.LifecycleBeanPostProcessor;
+import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.util.Factory;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Created by wb264139 on 2017/5/22.
+ */
+@Configuration
+public class ShiroConfiguration {
+
+    private static Map<String, String> filterChainDefinitionMap = new LinkedHashMap<String, String>();
+
+    @Bean(name = "ShiroRealmImpl")
+    public ShiroRealmImpl getShiroRealm() {
+        return new ShiroRealmImpl();
+    }
+
+    @Bean(name = "shiroEhcacheManager")
+    public EhCacheManager getEhCacheManager() {
+        EhCacheManager em = new EhCacheManager();
+        em.setCacheManagerConfigFile("classpath:ehcache-shiro.xml");
+        return em;
+    }
+
+    @Bean(name = "lifecycleBeanPostProcessor")
+    public LifecycleBeanPostProcessor getLifecycleBeanPostProcessor() {
+        return new LifecycleBeanPostProcessor();
+    }
+
+    @Bean
+    public DefaultAdvisorAutoProxyCreator getDefaultAdvisorAutoProxyCreator() {
+        DefaultAdvisorAutoProxyCreator daap = new DefaultAdvisorAutoProxyCreator();
+        daap.setProxyTargetClass(true);
+        return daap;
+    }
+
+    @Bean(name = "securityManager")
+    public DefaultWebSecurityManager getDefaultWebSecurityManager() {
+        DefaultWebSecurityManager dwsm = new DefaultWebSecurityManager();
+        dwsm.setRealm(getShiroRealm());
+        dwsm.setCacheManager(getEhCacheManager());
+        return dwsm;
+    }
+
+    @Bean
+    public AuthorizationAttributeSourceAdvisor getAuthorizationAttributeSourceAdvisor() {
+        AuthorizationAttributeSourceAdvisor aasa = new AuthorizationAttributeSourceAdvisor();
+        aasa.setSecurityManager(getDefaultWebSecurityManager());
+        return new AuthorizationAttributeSourceAdvisor();
+    }
+
+    @Bean(name = "shiroFilter")
+    public ShiroFilterFactoryBean getShiroFilterFactoryBean() {
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        shiroFilterFactoryBean
+                .setSecurityManager(getDefaultWebSecurityManager());
+        shiroFilterFactoryBean.setLoginUrl("/user/login");
+        shiroFilterFactoryBean.setSuccessUrl("http://localhost:8989/pages/main.html");
+        filterChainDefinitionMap.put("/exception/create", "authc");
+        filterChainDefinitionMap.put("/**", "anon");
+        shiroFilterFactoryBean
+                .setFilterChainDefinitionMap(filterChainDefinitionMap);
+        return shiroFilterFactoryBean;
+    }
+
+}
